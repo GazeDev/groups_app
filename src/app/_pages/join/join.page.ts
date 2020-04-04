19d776e7e099cf305ee418187ca9817d3bcc9f15
Daniel Kim <ddk1@andrew.cmu.edu>
2020-04-04 17:53:16 -0500

    Saving to fix other branch

--- src/app/_pages/join/join.page.ts
+++ src/app/_pages/join/join.page.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { ApiService } from '_services/api.service';
 
+import { Group } from '_models/group.model';
+import { environment } from '_environment';
+
 @Component({
   selector: 'join-page',
   templateUrl: 'join.page.html',
@@ -8,10 +11,21 @@ import { ApiService } from '_services/api.service';
 })
 export class JoinPage {
   public housingGroups: string[] = ['Pittsburgh', 'Oakland', 'Shadyside', 'Squirrel Hill', 'Lawrenceville', 'Southside', 'East Liberty', 'Strip District', 'Mount Washington', 'Downtown', 'Duquesne Heights'];
+
+  public env: any;
+  public groups: any = [];
   constructor(
     private apiService: ApiService,
   ) {
+    this.env = environment;
 
+    // Gets the group from the API and assigned them to this.groups
+    this.apiService.getGroups().subscribe( res => {
+      this.groups = res;
+    },
+    err => {
+      console.log('err', err);
+    });
   }
 
   ngOnInit() {
