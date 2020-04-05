98d9270457da7e875c7562b0d6d75e7d4908f87b
Daniel Kim <ddk1@andrew.cmu.edu>
2020-04-04 19:47:24 -0500

    Updated join page so that all currently created groups are displayed for the selection buttons rather than the dummy data

--- src/app/_pages/join/join.page.ts
+++ src/app/_pages/join/join.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from '_services/index';
 import { ApiService } from '_services/api.service';
 
 import { Group } from '_models/group.model';
@@ -10,23 +11,45 @@ import { environment } from '_environment';
   styleUrls: ['join.page.scss'],
 })
 export class JoinPage {
-  public housingGroups: string[] = ['Pittsburgh', 'Oakland', 'Shadyside', 'Squirrel Hill', 'Lawrenceville', 'Southside', 'East Liberty', 'Strip District', 'Mount Washington', 'Downtown', 'Duquesne Heights'];
+  // public housingGroups: string[] = ['Pittsburgh', 'Oakland', 'Shadyside', 'Squirrel Hill', 'Lawrenceville', 'Southside', 'East Liberty', 'Strip District', 'Mount Washington', 'Downtown', 'Duquesne Heights'];
 
   public env: any;
-  public groups: any = [];
+  public allGroups: any = [];
   constructor(
-    private apiService: ApiService,
+    public authService: AuthenticationService,
+    public apiService: ApiService,
   ) {
     this.env = environment;
 
     // Gets the group from the API and assigned them to this.groups
     this.apiService.getGroups().subscribe( res => {
-      this.groups = res;
+      this.allGroups = res;
     },
     err => {
       console.log('err', err);
     });
   }
+
+
+  // createGroup(group): void {
+  //   this.apiService.createGroup(group).subscribe(
+  //     groupResponse => {
+  //       let groupId = groupResponse.body.id; // Unsure if we need body
+  //       // this.displayLandlordCreatedToast(landlordId); // Leave for later
+  //       // this.form.reset();
+  //       // this.formDirective.resetForm();
+  //     },
+  //     // err => {
+  //     //   if (err.status == 422) {
+  //     //     this.alertService.action({
+  //     //       data: {
+  //     //         message: 'A group with that title already exists.',
+  //     //       }
+  //     //     });
+  //     //   }
+  //     // });
+  //   )
+  // }
 
   ngOnInit() {
 
