3e227005472318a2b2a50f962cb5ac39f0bf6862
Daniel Kim <ddk1@andrew.cmu.edu>
2020-04-09 19:26:24 -0500

    Updated Group model to use string instead of text

--- src/app/_models/group.model.ts
+++ src/app/_models/group.model.ts
@@ -2,7 +2,7 @@ export class Group {
   id?: string; // guid
   title?: string;
   short_description?: string;
-  description?: text;
+  description?: string;
   admin?: string; // guid
   posts?: string; // guid[,guid][,guid]
   metadata?: any; // metadata object with more info
